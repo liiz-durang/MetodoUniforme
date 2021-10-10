6fb102a37d38e58ea701d1b6d21805ed3b5e1125
lizduran <lizeth.durang@hotmail.com>
2021-10-10 09:43:34 -0500

    Grafo de ciudades y BFS

--- grafo.py
+++ grafo.py
@@ -4,18 +4,17 @@ import sys
 import math
 
 class Vertice:
-    def __init__(self, nombre ):
+    def __init__(self, nombre):
         self.nombre = nombre
-        self.vecinos = []
+        #Diccionario {verticeVecino, distanciaAEl}
+        self.vecinos = {}
         self.color = "white"
         self.padre = None
         self.distancia = sys.maxsize
-        self.latitud = latitud
-        self.longitud= longitud
 
-    def AgregarVecino(self, vertice):
+    def agregarVecino(self, vertice, distancia):
         if vertice not in self.vecinos:
-            self.vecinos.append(vertice)
+            self.vecinos[vertice] = distancia
             #print("Se agrego vecino", vertice)
         else:
             print("No se agrego vecino, ya esta en su lista de adyacencia")
@@ -30,11 +29,13 @@ class Vertice:
 class Grafo:
 
     def __init__(self):
+        # {nombre, objetoVertice}
         self.vertices = {}
 
-    def AgregarVertice(self, nombreVertice):
+    def agregarVertice(self, nombreVertice):
         if nombreVertice in self.vertices:
             print("Error: Ya existe el vertice en el grafo")
+            raise ValueError("Error: Ya existe el vertice en el grafo")
             return False
 
         #Si no existe en el grafo, Crear un vertice 
@@ -45,24 +46,149 @@ class Grafo:
         return True
         
 
-    def AgregarArista(self, verticeNombre1, verticeNombre2):
+    def agregarArista(self, verticeNombre1, verticeNombre2, distancia):
         #Si ya existen los vertices dentro del grafo, creara las relaciones
         if verticeNombre1 in self.vertices and verticeNombre2 in self.vertices:
-            
+
+            #Los recupera del diccionario de vertices
             vertice1 = self.vertices[verticeNombre1]
             vertice2 = self.vertices[verticeNombre2]
 
-          
-            self.vertices[vertice1.nombre].AgregarVecino(vertice2)
-           
-            self.vertices[vertice2.nombre].AgregarVecino(vertice1)
+            self.vertices[vertice1.nombre].agregarVecino(vertice2, distancia)
+            self.vertices[vertice2.nombre].agregarVecino(vertice1, distancia)
             return True
         else:
+            raise ValueError("Error: Ya existe la relacion de vertices en el grafo")
             return False
 
     def __str__(self):
         s = ""
+        i = 0
         for nombreVertice in self.vertices.keys():
-            s = s + nombreVertice + " -> " + \
+            s = s + str(i) + ") " + nombreVertice + " -> " + \
                 str(self.vertices[nombreVertice].vecinos) + "\n"
+            i = i+1
         return s
+
+    #Busqueda en anchura
+    def BFS (self, verticeStart, verticeFinal):
+        
+        for u in self.vertices.values ():
+            u.color = "white"
+            u.padre = None
+
+        verticeStart.color = "gris"
+        verticeStart.padre = None
+
+        queue = [ ]
+        queue.append ( verticeStart ) #Se le agrega la referencia al verticeStart
+
+        while len (queue) > 0 :   
+            u = queue.pop (0)
+
+            for v in u.vecinos :
+                
+                if v.color == "white" :
+                    v.color= "gris"
+                    v.padre = u 
+                    queue.append ( v )
+
+            u.color = "negro"
+
+            if u is verticeFinal:
+                break
+    
+    def encontrarCaminoBFS (self, nombreStart, nombreFinal):
+        distancia = 0
+
+        padres = []
+        #Del diccionario de vertices, recupera los objetos vertices que se llamen asi.
+        if nombreStart  not in self.vertices:
+            print(nombreStart + " no existe en el mapa")
+            exit()
+        elif nombreFinal not in self.vertices:
+            print( nombreFinal + " no existe en el mapa")
+            exit()
+        
+        verticeStart = self.vertices[nombreStart] 
+        verticeFinal = self.vertices[nombreFinal] 
+        self.BFS(verticeStart, verticeFinal)
+        
+        padres.append (verticeFinal)
+        aux = Vertice ("aux")
+        aux = verticeFinal.padre 
+        distancia = verticeFinal.vecinos.get(aux)
+
+        while aux.padre != None: 
+            padres.append (aux)
+            distancia = distancia + aux.vecinos.get(aux.padre)
+            aux = aux.padre 
+        padres.append(nombreStart)
+        padres = padres[::-1]
+        return distancia , padres
+
+    #Método de costo uniforme
+    def costoUniforme (self, verticeStart, verticeFinal):
+        
+        for u in self.vertices.values ():
+            u.color = "white"
+            u.padre = None
+            u.distancia = sys.maxsize
+        
+        #recuperar del diccionario, el objeto cuya llave sea nombreStart
+        verticeStart.color = "gris"
+        verticeStart.padre = None
+        verticeStart.distancia = 0
+
+        queue = [ ]
+        queue.append ( verticeStart ) #Se le agrega la referencia al verticeStart
+        while len (queue) > 0 :   
+            u = queue.pop (0)
+                
+            for v in u.vecinos :
+                distanciaMin = sys.maxsize
+                if v.color == "white" :
+                    v.color= "gris"
+                    v.padre = u 
+                    v.distancia = v.distancia + u.vecinos.get(v)
+                    queue.append ( v )
+
+
+
+            u.color = "negro"
+
+            if u is verticeFinal:
+                break
+
+
+    
+    def encontrarCaminoCostoUniforme (self, nombreStart, nombreFinal):
+        distancia = 0
+
+        padres = []
+        #Del diccionario de vertices, recupera los objetos vertices que se llamen asi.
+        if nombreStart  not in self.vertices:
+            print(nombreStart + " no existe en el mapa")
+            exit()
+        elif nombreFinal not in self.vertices:
+            print( nombreFinal + " no existe en el mapa")
+            exit()
+        
+        verticeStart = self.vertices[nombreStart] 
+        verticeFinal = self.vertices[nombreFinal] 
+        self.costoUniforme(verticeStart, verticeFinal)
+
+        padres.append (verticeFinal)
+        aux = Vertice ("aux")
+        aux = verticeFinal.padre 
+        distancia = verticeFinal.vecinos.get(aux)
+        '''
+        while aux.padre != None: 
+            padres.append (aux)
+            distancia = distancia + aux.vecinos.get(aux.padre)
+            aux = aux.padre 
+        padres.append(nombreStart)
+        padres = padres[::-1]
+        return distancia , padres
+
+    '''
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-#Método que lee el archivo csv
+from grafo import *
+
+#MLee el archivo de ciudades. 
+# retorna un arreglo de arreglos. 
+# Cada elemento del arreglo es la relación entre 2 ciudades.
 def obtenerInfo():
-    data = []
-    with open ('ciudades.csv') as file1:
-        
-        readlinea = file1.readline()
-
-        for linea in readlinea: 
-            print(linea, end='')
-
-        
-
-
-if __name__ == "__main__":
-   
-  
-    print("\n")
-    print("            ====PROYECTO ====")
-    print("      ===ALGORITMO DE COSTO UNIFORME====")
 
     try:
         file1 = open ('ciudades.csv', 'r')
@@ -24,4 +11,58 @@ if __name__ == "__main__":
         print ("Error al abrir archivo: ", err)
         exit (0)
 
-    mapa = obtenerInfo() 
+    #arreglo de arreglos
+    data = []
+    with open ('ciudades.csv') as file1:
+        for linea in file1: 
+            item = linea.split(',')
+            #quitar el salto de linea del ultimo elemento
+            item[2] = item[2][:-1]
+            data.append(item)
+
+    file1.close()        
+    
+    return data
+
+#Llena el grafo con los datos del arreglo data
+def llenarGrafo(grafo):
+
+    #Agregar ciudades como vertices
+    for i in arrMapa:
+        if i[0] not in grafoMapa.vertices:
+            grafoMapa.agregarVertice(i[0])
+            
+        if i[1] not in grafoMapa.vertices:
+            grafoMapa.agregarVertice(i[1])
+        
+    #Agregar relación entre ciudades
+    for i in arrMapa:
+        grafoMapa.agregarArista(i[0], i[1], int(i[2]))
+
+if __name__ == "__main__":
+
+    print("\n")
+    print("             ====PROYECTO ====")
+    print("      ===ALGORITMO DE COSTO UNIFORME====")
+    print("\n")
+
+    #1. Obtener info de las ciudades
+    arrMapa = obtenerInfo() 
+    
+    #2. Crear el grafo 
+    grafoMapa = Grafo()
+
+    #3. Llenar el grafo con las ciudades
+    llenarGrafo(grafoMapa)
+
+    #4. BFS
+    #distacia, padres = grafoMapa.encontrarCaminoBFS("Arad", "Bucharest")
+    #print(padres)
+    #print("Distancia: " + str(distacia))
+
+    #5. Costo Uniforme
+    #distancia1, padres1 = grafoMapa.encontrarCaminoCostoUniforme("Arad", "Bucharest")
+    #print(padres1)
+    #print("Distancia: " + str(distancia1))
+    grafoMapa.encontrarCaminoCostoUniforme("Arad", "Bucharest")
+    
